841d8da1e114d28db5f43271e8f6a77b8fbb8481
benhoyle <benjhoyle@gmail.com>
2017-03-23 20:07:03 +0000

    Moving test into main dir

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,9 @@ BASE_DIR = os.path.split(
 LOG_LEVEL = 'INFO'
 
 # SQLAlchemy.
-SQLALCHEMY_DATABASE_URI  = 'sqlite:///' + BASE_DIR + '/db/website.db'
+SQLALCHEMY_DATABASE_URI  = (
+    'sqlite:///' + BASE_DIR + '/benhoyle/db/website.db'
+    )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Cookie Settings
